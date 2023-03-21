f597deea78d6c1ecc2be616919493d9f476fd71a
Melah Ghislain <ghislain.melah@nanodev.ca>
2023-03-22 00:00:26 +0100

    feat: user validation schema"

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,4 +1,17 @@
+import Joi from "joi";
 import { User } from "./types";
+
+const userValidationSchema = Joi.object().keys({
+    username: Joi.string().min(2).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(4).required(),
+    picture: Joi.string(),
+    cartItems: Joi.array(),
+    favorites: Joi.array(),
+    firstName: Joi.string(),
+    lastName: Joi.string(),
+    role: Joi.string()
+})
 
 export function isUserValid(user: User): boolean{
  return true
